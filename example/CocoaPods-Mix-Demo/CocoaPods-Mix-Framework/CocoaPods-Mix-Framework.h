96b5a3eb8d154e4a130214559cd48bab830244cc
Florent Vilmart <florentvilmart@me.com>
2017-05-09 15:22:41 -0400

    Adds demo project

---
+++ example/CocoaPods-Mix-Demo/CocoaPods-Mix-Framework/CocoaPods-Mix-Framework.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPods-Mix-Framework.h
+//  CocoaPods-Mix-Framework
+//
+//  Created by Florent Vilmart on 17-05-09.
+//  Copyright © 2017 ampme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaPods-Mix-Framework.
+FOUNDATION_EXPORT double CocoaPods_Mix_FrameworkVersionNumber;
+
+//! Project version string for CocoaPods-Mix-Framework.
+FOUNDATION_EXPORT const unsigned char CocoaPods_Mix_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPods_Mix_Framework/PublicHeader.h>
+
+
